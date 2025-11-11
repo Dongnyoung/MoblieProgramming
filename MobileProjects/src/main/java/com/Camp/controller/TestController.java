5ce93434473926b802d4d024c442a5c0bc7c6f9c
Dongnyoung <leedo7182@naver.com>
2025-11-11 23:53:09 +0900

    test: 테스트

--- MobileProjects/src/main/java/com/Camp/controller/TestController.java
+++ MobileProjects/src/main/java/com/Camp/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.Camp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
-    @GetMapping("/api/hello") public String hello(){ return "Hello from Spring"; }
+    @GetMapping("/hello") public String hello(){ return "Hello from Spring"; }
 }
